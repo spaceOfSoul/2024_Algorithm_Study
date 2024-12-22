cbf5f50633c843f95c0303612df9a98949b9d8a8
YuchanLee2 <106137298+YuchanLee2@users.noreply.github.com>
2024-12-22 20:59:57 +0900

    올바른_괄호

---
+++ yuchan/1216_1222/Solution2.java
@@ -0,0 +1,59 @@
+
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int count = 0, i = 0;
+        Queue<sub>queue = new LinkedList<>();
+        if(s.charAt(i) == '(')
+        {
+            queue.offer(new sub(s.charAt(i), 1));
+        }
+        else
+        {
+            return false;
+        }
+        while(!queue.isEmpty())
+        {
+            sub temp = queue.poll();
+            count = temp.count;
+            if(i<s.length()-1)
+            {
+                i++;
+            }
+            else
+            {
+                break;
+            }
+            if(s.charAt(i) == '(')
+            {
+                queue.offer(new sub(s.charAt(i), temp.count+1));
+                
+            }
+            if(s.charAt(i) == ')')
+            {
+                queue.offer(new sub(s.charAt(i), temp.count-1));
+            }
+            if(temp.count < 0)
+            {
+                return false;
+            }
+        }
+        if(count == 0)
+        {
+            return answer;
+        }
+        else{
+            return false;
+        }
+    }
+}
+class sub{
+     char temp;
+     int count;
+    sub(char temp, int count)
+    {
+        this.temp = temp;
+        this.count = count;
+    }
+}
